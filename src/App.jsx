653acbe1b328503a2a804ef9a5613c4467ad4c09
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-10-22 04:43:31 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useEffect, Suspense } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Layout from "./components/Layout/Layout";
 import ContactsPage from "./Pages/ContactsPage/ContactsPage";
-import NotFound from "./Pages/NotFound/NotFound";
+
 import LoginPage from "./components/LoginForm/LoginForm";
 import RegistrationPage from "./components/RegistrationForm/RegistrationForm.jsx";
 import { refreshUser } from "./redux/auth/operations.js";
@@ -59,7 +59,6 @@ function App() {
               }
             />
           </Route>
-          <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
     </div>
