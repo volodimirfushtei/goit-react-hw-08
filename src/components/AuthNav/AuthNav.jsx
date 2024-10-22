d1b4ff5c24212d287c3ce95a623fd9953ab0027d
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-10-22 22:40:53 +0300

    add

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -6,32 +6,6 @@ import Avatar from "@mui/material/Avatar";
 import s from "./AuthNav.module.css";
 import clsx from "clsx";
 
-function stringToColor(string) {
-  if (!string) return "#000"; // Повертаємо чорний колір, якщо рядок порожній або null
-  let hash = 0;
-  for (let i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
-
-  let color = "#";
-  for (let i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.slice(-2);
-  }
-
-  return color;
-}
-
-function stringAvatar(name) {
-  if (!name) return {}; // Повертаємо пустий об'єкт, якщо ім'я відсутнє
-  return {
-    sx: {
-      bgcolor: stringToColor(name),
-    },
-    children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
-  };
-}
-
 const AuthNav = () => {
   const user = useSelector(selectUser);
   const isLoggedIn = useSelector(selectIsLoggedIn);
@@ -43,7 +17,7 @@ const AuthNav = () => {
   return (
     <div className={s.name_wrapp}>
       <h1 className={s.h1}>Phonebook</h1>
-      <Avatar {...stringAvatar(user.name)} />
+      <Avatar />
       <div className={s.name_wrapp}>
         {isLoggedIn ? (
           <div className={s.nameuser}>{user.name}</div>
--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -6,31 +6,7 @@ import s from "./UserMenu.module.css";
 import { logout } from "../../redux/auth/operations.js";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../redux/auth/selectors.js";
-function stringToColor(string) {
-  if (!string) return "#000"; // Повертаємо чорний колір, якщо рядок порожній або null
-  let hash = 0;
-  for (let i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
 
-  let color = "#";
-  for (let i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.slice(-2);
-  }
-
-  return color;
-}
-
-function stringAvatar(name) {
-  if (!name) return {}; // Повертаємо пустий об'єкт, якщо ім'я відсутнє
-  return {
-    sx: {
-      bgcolor: stringToColor(name),
-    },
-    children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
-  };
-}
 const UserMenu = () => {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
@@ -42,7 +18,7 @@ const UserMenu = () => {
   return (
     <div className={s.wrapper}>
       <h1 className={s.h1}>Phonebook</h1>
-      <Avatar {...stringAvatar(user.name)} />
+      <Avatar />
       <div className={s.nameuser}>{user.name}</div>
       <Button variant="contained" color="secondary" onClick={handleLogout}>
         Logout
